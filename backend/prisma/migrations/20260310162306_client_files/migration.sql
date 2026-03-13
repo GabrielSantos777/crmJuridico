baee955efcf3e9a3326846cf9b67986c5052f06e
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-03-13 00:05:17 -0300

    correção de migration duplicado

--- backend/prisma/migrations/20260310162306_client_files/migration.sql
+++ backend/prisma/migrations/20260310162306_client_files/migration.sql
@@ -1,22 +1,4 @@
 -- CreateTable
-CREATE TABLE "Appointment" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT,
-    "startAt" TIMESTAMP(3) NOT NULL,
-    "endAt" TIMESTAMP(3) NOT NULL,
-    "status" "AppointmentStatus" NOT NULL DEFAULT 'SCHEDULED',
-    "channel" TEXT,
-    "phone" TEXT,
-    "leadId" TEXT,
-    "clientId" TEXT,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "ClientFile" (
     "id" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
@@ -30,14 +12,6 @@ CREATE TABLE "ClientFile" (
     CONSTRAINT "ClientFile_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
-CREATE INDEX "Appointment_startAt_endAt_idx" ON "Appointment"("startAt", "endAt");
-
 -- AddForeignKey
-ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_leadId_fkey" FOREIGN KEY ("leadId") REFERENCES "Lead"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "ClientFile" ADD CONSTRAINT "ClientFile_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ClientFile" ADD CONSTRAINT "ClientFile_clientId_fkey"
+FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
