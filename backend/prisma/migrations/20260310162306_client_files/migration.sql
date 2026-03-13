f826a894c2a1c907f7aac53fcb903e15cdc92167
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-03-13 00:02:53 -0300

    correção de migration duplicado

--- backend/prisma/migrations/20260310162306_client_files/migration.sql
+++ backend/prisma/migrations/20260310162306_client_files/migration.sql
@@ -1,46 +1,3 @@
--- CreateEnum
--- CREATE TYPE "ConversationStatus" AS ENUM ('OPEN', 'CLOSED');
-
--- CreateEnum
--- CREATE TYPE "MessageDirection" AS ENUM ('IN', 'OUT');
-
--- CreateEnum
--- CREATE TYPE "AppointmentStatus" AS ENUM ('SCHEDULED', 'CANCELLED', 'COMPLETED');
-
--- AlterTable
--- ALTER TABLE "Client" ADD COLUMN     "phoneNormalized" TEXT;
-
--- -- AlterTable
--- ALTER TABLE "Lead" ADD COLUMN     "phoneNormalized" TEXT;
-
--- CreateTable
-CREATE TABLE "Conversation" (
-    "id" TEXT NOT NULL,
-    "channel" TEXT NOT NULL DEFAULT 'WHATSAPP',
-    "phone" TEXT NOT NULL,
-    "name" TEXT,
-    "leadId" TEXT,
-    "clientId" TEXT,
-    "status" "ConversationStatus" NOT NULL DEFAULT 'OPEN',
-    "lastMessageAt" TIMESTAMP(3),
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Conversation_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Message" (
-    "id" TEXT NOT NULL,
-    "conversationId" TEXT NOT NULL,
-    "direction" "MessageDirection" NOT NULL,
-    "text" TEXT NOT NULL,
-    "meta" JSONB,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateTable
 CREATE TABLE "Appointment" (
     "id" TEXT NOT NULL,
@@ -74,19 +31,7 @@ CREATE TABLE "ClientFile" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Conversation_channel_phone_key" ON "Conversation"("channel", "phone");
-
--- CreateIndex
 CREATE INDEX "Appointment_startAt_endAt_idx" ON "Appointment"("startAt", "endAt");
-
--- AddForeignKey
-ALTER TABLE "Conversation" ADD CONSTRAINT "Conversation_leadId_fkey" FOREIGN KEY ("leadId") REFERENCES "Lead"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Conversation" ADD CONSTRAINT "Conversation_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Message" ADD CONSTRAINT "Message_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "Conversation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_leadId_fkey" FOREIGN KEY ("leadId") REFERENCES "Lead"("id") ON DELETE SET NULL ON UPDATE CASCADE;
